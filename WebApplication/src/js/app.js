9b9f7c99d16aec7e192290c8389b70cad88ca01c
jvolpe <juan.volpe@globant.com>
2016-02-19 10:29:31 -0300

    Render of CompositeView

--- WebApplication/src/js/app.js
+++ WebApplication/src/js/app.js
@@ -1,21 +1,66 @@
-define(function (require) {
-
+define(function(require) {
 
     var Backbone = require('backbone');
     var Marionette = require('backbone.marionette');
 
-    var TodoList = Marionette.LayoutView.extend({
+    var ToDoModel = require('./models/todo');
+
+    var ToDo = Marionette.LayoutView.extend({
+      tagName: 'li',
+      template: require('hbs!./templates/todoitem')
+    });
+
+    var TodoList = Marionette.CompositeView.extend({
       el: '#app-hook',
-      template: require('hbs!./templates/layout')
+      template: require('hbs!./templates/todolist'),
+
+      childView: ToDo,
+      childViewContainer: 'ul',
+
+      ui: {
+        assignee: '#id_assignee',
+        form: 'form',
+        text: '#id_text'
+      },
+
+      triggers: {
+        'submit @ui.form': 'add:todo:item'
+      },
+
+      collectionEvents: {
+        add: 'itemAdded'
+      },
+
+    //   modelEvents: {
+    //     change: 'render'
+    //   },
+
+      onAddTodoItem: function() {
+        // debugger
+        this.model.set({
+          assignee: this.ui.assignee.val(),
+          text: this.ui.text.val()
+        }, {validate: true});
+
+        var items = this.model.pick('assignee', 'text');
+        this.collection.add(items);
+      },
+
+      itemAdded: function() {
+        this.render();
+        this.model.set({
+          assignee: '',
+          text: ''
+        });
+      }
     });
 
     var todo = new TodoList({
-      model: new Backbone.Model({
-        items: [
-          {assignee: 'Scott', text: 'Write a book about Marionette'},
-          {assignee: 'Andrew', text: 'Do some coding'}
-        ]
-      })
+      collection: new Backbone.Collection([
+        {assignee: 'Scott', text: 'Write a book about Marionette'},
+        {assignee: 'Andrew', text: 'Do some coding'}
+      ]),
+      model: new ToDoModel()
     });
 
     todo.render();
